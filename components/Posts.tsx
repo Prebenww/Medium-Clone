01453a6981f12a5830c8805f0dbac199c6275996
preben wulff <prebenw@live.no>
2022-03-08 17:59:24 +0100

    sanity integrasjon

---
+++ components/Posts.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link"
+import {Post} from "../typing";
+
+interface Props {
+    posts: [Post];
+}
+
+const Posts = ( { posts }: Props) => {
+    return (
+        <div>
+            {posts.map((post) => (
+                <Link key={post._id} href={`/post/${post.slug.current}`}>
+                    <div>
+                        <h1>Dette er en post</h1>
+                    </div>
+
+                </Link>
+            ))}
+        </div>
+    )
+}
+export default Posts;
